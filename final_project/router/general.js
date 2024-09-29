a0833975df8d7f03d53ce10c1289175dfdcc8d01
Polly Yospan <porgroovy@gmail.com>
2024-09-29 12:48:47 -0400

    Complete the code for getting book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -73,7 +73,19 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Get the ISBN from the request parameters
+  const isbn = req.params.isbn;
+
+  // Check if the book exists by using the ISBN as the key
+  const book = books[isbn];
+
+  // If the book is found, return its reviews
+  if (book) {
+    return res.status(200).json(book.reviews);
+  } else {
+    // If the book is not found, return a 404 error
+    return res.status(404).json({ message: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
